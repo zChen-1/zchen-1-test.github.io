286b5cbfbb4f6fcd56fc4c1e84f0d4658c41cccd
Chen <73560896+zChen-1@users.noreply.github.com>
2023-05-04 18:35:55 -0700

    Update register.php

--- register.php
+++ register.php
@@ -1,37 +1,11 @@
-// register.php
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  // Connect to the database
-  $conn = new SQLite3('users.db');
-  
-  $sql = 'CREATE TABLE users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
-)';
-$result = $conn->exec($sql);
+$username = $_POST['username'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$email = $_POST['email'];
 
-  // Get user input
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
+$file = fopen('users.txt', 'a');
+fwrite($file, "$username:$password:$email\n");
+fclose($file);
 
-  // Hash the password for security
-  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-  // Insert the user data into the "users" table
-  $stmt = $conn->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
-  $stmt->bindValue(':name', $name, SQLITE3_TEXT);
-  $stmt->bindValue(':email', $email, SQLITE3_TEXT);
-  $stmt->bindValue(':password', $hashed_password, SQLITE3_TEXT);
-  $result = $stmt->execute();
-
-  // Close the database connection
-  $conn->close();
-
-  // Redirect the user to login page
-  header('Location: login.php');
-  exit;
-}
+echo 'Registration successful!';
 ?>
