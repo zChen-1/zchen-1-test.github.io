10919ebf2b3846a27550afd69364e6b8a1081153
Chen <73560896+zChen-1@users.noreply.github.com>
2023-05-04 18:15:38 -0700

    Update register.php

--- register.php
+++ register.php
@@ -1,19 +1,26 @@
+// register.php
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // Connect to the database
   $conn = new SQLite3('users.db');
 
   // Get user input
-  $username = $_POST['username'];
+  $name = $_POST['name'];
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  // Insert user data into database
-  $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
-  $conn->exec($sql);
+  // Hash the password for security
+  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+  // Insert the user data into the "users" table
+  $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$hashed_password')";
+  $result = $conn->query($sql);
 
   // Close the database connection
   $conn->close();
 
-  // Redirect to login page
-  header("Location: login.php");
+  // Redirect the user to login page
+  header('Location: login.php');
+  exit;
+}
 ?>
