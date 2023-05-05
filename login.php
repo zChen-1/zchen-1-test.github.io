e269d2dba8b904c971bd296b5c048caf5abf1e37
Chen <73560896+zChen-1@users.noreply.github.com>
2023-05-04 18:18:17 -0700

    Update login.php

--- login.php
+++ login.php
@@ -4,14 +4,29 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $email = $_POST['email'];
   $password = $_POST['password'];
-  // Validate user input
+
+  // Connect to the database
+  $conn = new SQLite3('users.db');
+
   // Check if user exists in database
-  // Verify password
-  // If successful, set session variables and redirect to dashboard
-  $_SESSION['user_id'] = $user_id;
-  $_SESSION['email'] = $email;
-  header('Location: dashboard.php');
-  exit;
-  // If not, return error message
+  $stmt = $conn->prepare('SELECT id, name, email, password FROM users WHERE email = :email');
+  $stmt->bindValue(':email', $email, SQLITE3_TEXT);
+  $result = $stmt->execute();
+  $row = $result->fetchArray(SQLITE3_ASSOC);
+
+  if ($row && password_verify($password, $row['password'])) {
+    // If successful, set session variables and redirect to dashboard
+    $_SESSION['user_id'] = $row['id'];
+    $_SESSION['name'] = $row['name'];
+    $_SESSION['email'] = $row['email'];
+    header('Location: dashboard.php');
+    exit;
+  } else {
+    // If not, return error message
+    $error = 'Invalid email or password.';
+  }
+
+  // Close the database connection
+  $conn->close();
 }
 ?>
